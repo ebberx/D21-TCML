f0396f8de40a29a7c2770c892b10c9addbd3a7d0
Ebberx <esben_dahl@hotmail.com>
2022-10-10 22:35:44 +0200

    Debug print

--- TextClassificationWPF/MainWindow.xaml.cs
+++ TextClassificationWPF/MainWindow.xaml.cs
@@ -4,14 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Diagnostics;
 using TextClassificationWPF.Controller;
 using TextClassificationWPF.Domain;
 
@@ -25,30 +18,25 @@ namespace TextClassificationWPF
         public MainWindow() {
             InitializeComponent();
 
-            /////* Taken from Program.cs */
             KnowledgeBuilder nb = new KnowledgeBuilder();
 
             // initiate the learning process
-
             nb.Train();
 
             // getting the (whole) knowledge found in ClassA and in ClassB
             Knowledge k = nb.GetKnowledge();
-
 
             // get a part of the knowledge - here just for debugging
             BagOfWords bof = k.GetBagOfWords();
 
             List<string> entries = bof.GetEntriesInDictionary();
 
-
-            Console.WriteLine("Showing the list of entries in the BagOfWords (initially with wrong entries) :");
+            Debug.WriteLine("Showing the list of entries in the BagOfWords (initially with wrong entries) :");
             foreach (string entry in entries) {
-                Console.WriteLine(entry);
+                Debug.WriteLine(entry);
             }
 
             Console.Read();
-            ////
         }
     }
 }
