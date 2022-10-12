3a145106ff3d5aba1e390200e09177e034a5ad51
Ebberx <esben_dahl@hotmail.com>
2022-10-12 22:44:59 +0200

    Created GUI for training and classifying an arbitary piece of text

--- TextClassificationWPF/MainWindow.xaml.cs
+++ TextClassificationWPF/MainWindow.xaml.cs
@@ -12,21 +12,23 @@ namespace TextClassificationWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        KnowledgeBuilder nb;
+
         public MainWindow() {
             InitializeComponent();
+        }
 
-            KnowledgeBuilder nb = new KnowledgeBuilder(new List<string>() { "ClassA", "ClassB" });
-
-            // initiate the learning process
+        private void OnClickTrain(object sender, RoutedEventArgs e) {
+            nb = new KnowledgeBuilder(new List<string>() { "ClassA", "ClassB" });
             nb.Train();
+            MessageBox.Show("Training has completed", "Info message");
+        }
 
+        private void OnClickClassify(object sender, RoutedEventArgs e) {
             // getting the (whole) knowledge found in ClassA and in ClassB
             Knowledge k = nb.GetKnowledge();
 
-            // get a part of the knowledge - here just for debugging
-            BagOfWords bof = k.GetBagOfWords();
-
-            List<string> entries = bof.GetEntriesInDictionary();
+            List<string> entries = k.GetBagOfWords().GetEntriesInDictionary();
 
             Debug.WriteLine("Showing the list of entries in the BagOfWords (initially with wrong entries) :");
             foreach (string entry in entries) {
